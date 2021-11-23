5ce32e20c5ac2beb07a3f2565bed4faa2657c062
Jbullzy <bullington.jack@gmail.com>
2021-11-23 07:49:42 -0700

    Updated with attempts at finding soup links

--- search.py
+++ search.py
@@ -1,25 +1,36 @@
 import requests, sys, webbrowser, bs4
 from bs4 import BeautifulSoup
+from os import write
 
 print("searching google")
 res = requests.get('https://google.com/search?q=ASU')
+res.raise_for_status()
 
-print("creating soup variable")
+# print("printing res variable")
+# print(res)
+
+print("creating soup as a variable")
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 
-print("selecting links from the results")
-links = soup.select('.r a')
+# print("printing soup.a")
+# print(soup.a)
 
-for i in links:
-    print("-----printing-----")
-    print(i.text)
-    break
+print("printing soup.h3")
+print(soup.h3)
 
-# print(links)
+# print("printing soup.p")
+# print(soup.p)
 
-###
-#   print("opening five links from the result")
-#    openlinks = min(5, len(links))
-#    for i in range(openlinks):
-#        webbrowser.open('https://google.com'+links[i].get('href'))
+#print("printing prettified soup to a separate text file")
+# with open('prettified.txt', 'w') as f:
+#    write(print(soup.prettify), file=f)
+
+# print("selecting links from the results")
+# links = soup.select('.r a')
+
+# for i in links:
+   # print("-----printing-----")
+    # print(i.text)
+    # break
+
 
